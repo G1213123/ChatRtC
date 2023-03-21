432231a42fea4a4d2c5488d55859ce0df246911e
g1213123 <ngwingfung2136@yahoo.com.hk>
2023-03-21 18:14:26 +0800

    v0.1 test faiss

--- ingest.py
+++ ingest.py
@@ -1,5 +1,6 @@
 """This is the logic for ingesting Notion data into LangChain."""
 from pathlib import Path
+import time
 from langchain.text_splitter import CharacterTextSplitter
 import faiss
 from langchain.vectorstores import FAISS
@@ -8,7 +9,7 @@ import pickle
 from dotenv import load_dotenv
 import os
 import openai
-from ratelimiter import RateLimiter
+import tqdm
 
 load_dotenv()
 
@@ -24,22 +25,29 @@ for p in ps:
         data.append(f.read())
     sources.append(p)
 
-# Here we split the documents, as needed, into smaller chunks.
-# We do this due to the context limits of the LLMs.
-text_splitter = CharacterTextSplitter(chunk_size=1000, separator="\n")
-docs = []
-metadatas = []
-for i, d in enumerate(data):
-    splits = text_splitter.split_text(d)
-    docs.extend(splits)
-    metadatas.extend([{"source": sources[i]}] * len(splits))
+def chunks (data):
+    # Here we split the documents, as needed, into smaller chunks.
+    # We do this due to the context limits of the LLMs.
+    text_splitter = CharacterTextSplitter(chunk_size=1000, separator="\n")
+    for i, d in enumerate(data):
+        splits = text_splitter.split_text(d)
+        docs = splits
+        metadatas = [{"source": sources[i]}] * len(splits)
+        yield (docs, metadatas)
 
-@RateLimiter(max_calls=20, period=60)
 def upload():
     # Here we create a vector store from the documents and save it to disk.
-    store = FAISS.from_texts(docs, OpenAIEmbeddings(), metadatas=metadatas)
-    faiss.write_index(store.index, "docs.index")
-    store.index = None
+    for index, (doc, metadata) in tqdm.tqdm(enumerate(chunks(data))):
+        if index == 0:
+            store = FAISS.from_texts(doc, OpenAIEmbeddings(), metadatas=metadata)
+        else:
+            try:
+                store.add_texts(doc, metadata)
+            except:
+                time.sleep(60)
+                store.add_texts(doc, metadata)
+        faiss.write_index(store.index, "docs.index")
+        #store.index = None
     with open("faiss_store.pkl", "wb") as f:
         pickle.dump(store, f)
 
