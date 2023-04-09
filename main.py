7f2639b08abeb115c5451cd213976c68dd7bfe10
1213123 <ngwingfung2136@yahoo.com.hk>
2023-04-09 20:12:52 +0800

    0.4 change to pinecone

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import pickle
 import pathlib
 import platform
 import ingest
+from google.oauth2 import service_account
+from google.cloud import storage
 
 plt = platform.system()
 if plt == 'Windows': pathlib.PosixPath = pathlib.WindowsPath
@@ -46,6 +48,20 @@ chain = RetrievalQAWithClausesSourcesChain.from_llm( llm=ChatOpenAI( temperature
                                                      vectorstore=store,
                                                      k=2,
                                                      verbose=True )
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
+
+# Retrieve file contents.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def read_file(bucket_name, file_path):
+    bucket = client.bucket(bucket_name)
+    content = bucket.blob(file_path).download_as_string().decode("utf-8")
+    return content
 
 # From here down is all the StreamLit UI.
 st.set_page_config( page_title="ChatRTC loaded with TPDM", page_icon=":robot:", layout="wide" )
